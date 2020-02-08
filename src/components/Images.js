5a862b62e0c259f1331ec5b97bdbabfa3e6e3231
Joel Coddington <coddingtonjoel@gmail.com>
2020-02-07 23:12:33 -0800

    update
    added mockup images from Adobe XD to homepage

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,10 +1,12 @@
 import React from "react";
+import crop_one from "../images/Mockup/crop1.png";
+import crop_two from "../images/Mockup/crop2.png";
 
 const Images = () => {
     return (
         <div className="images">
-            IMAGES PLACEHOLDER [half / half] with diagonal line between two
-            images
+            <img className="images__croptwo" src={crop_two} alt="" />
+            <img className="images__cropone" src={crop_one} alt="" />
         </div>
     );
 };
