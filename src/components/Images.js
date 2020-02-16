61fcc2bec436e4b2750e074f464e58c4907bab94
Joel Coddington <coddingtonjoel@gmail.com>
2020-02-15 21:14:32 -0800

    image fix
    added new 1x and 2x images, added image responsiveness, and changed images and image positioning of mockups when scaled down

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,12 +1,38 @@
 import React from "react";
-import crop_one from "../images/Mockup/crop1.png";
-import crop_two from "../images/Mockup/crop2.png";
+import crop_one1x from "../images/Mockup/crop1.png";
+import crop_two1x from "../images/Mockup/crop2.png";
+import crop_one2x from "../images/Mockup/crop1@2x.png";
+import crop_two2x from "../images/Mockup/crop2@2x.png";
+import cropsm1_1x from "../images/Mockup/cropSm1@1x.png";
+import cropsm1_2x from "../images/Mockup/cropSm1@2x.png";
+import cropsm2_1x from "../images/Mockup/cropSm2@1x.png";
+import cropsm2_2x from "../images/Mockup/cropSm2@2x.png";
 
 const Images = () => {
     return (
         <div className="images">
-            <img className="images__croptwo" src={crop_two} alt="" />
-            <img className="images__cropone" src={crop_one} alt="" />
+            <picture>
+                <source
+                    srcset={`${cropsm2_1x} 1x, ${cropsm2_2x} 2x`}
+                    media="(max-width: 680px)"
+                />
+                <img
+                    className="images__croptwo"
+                    srcset={`${crop_two1x} 1x, ${crop_two2x} 2x`}
+                    alt=""
+                />
+            </picture>
+            <picture>
+                <source
+                    srcset={`${cropsm1_1x} 1x, ${cropsm1_2x} 2x`}
+                    media="(max-width: 680px)"
+                />
+                <img
+                    className="images__cropone"
+                    srcset={`${crop_one1x} 1x, ${crop_one2x} 2x`}
+                    alt=""
+                />
+            </picture>
         </div>
     );
 };
